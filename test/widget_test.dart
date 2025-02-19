4bfe939b17de8697a8639969930533d13e63058f
michael-k <michaelsure9@gmail.com>
2025-02-19 15:55:20 +0200

    Updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,175 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mqtt_ff_client/client_helper.dart';
+import 'package:mqtt_ff_client/mqtt_bridge.dart';
 
-import 'package:mqtt_ff_client/main.dart';
+void response(bool ok, String text, bool next) {
+  if (ok) {
+    print('Succeeded: $text');
+  }
+  else {
+    print('Failed:    $text');
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MQTTClientApp());
+  ClientHelper.initInstance();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Init', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge,isNotNull);
+    expect(bridge.state(), 'Disconnected');
   });
+
+  test('Connect', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+  });
+
+  test('Connect-Failed', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Failed');
+    expect(bridge.state(), 'Disconnected');
+  });
+
+  test('Connect-Succeeded', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+  });
+
+  test('AwaitSubscribe-Connect', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Connect');
+    expect(bridge.state(), 'AwaitSubscribe');
+  });
+
+  test('AwaitSubscribe-Subscribe', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+  });
+
+  test('AwaitSubscribe-Subscribe-Failed', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+    bridge.post('Failed');
+    expect(bridge.state(), 'AwaitSubscribe');
+  });
+
+  test('AwaitSubscribe-Subscribe-Succeeded', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitPublishing');
+  });
+
+  test('AwaitPublishing-Connect', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitPublishing');
+    bridge.post('Connect');
+    expect(bridge.state(), 'AwaitPublishing');
+  });
+
+  test('AwaitPublishing-Publish', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitPublishing');
+    bridge.post('Publish');
+    expect(bridge.state(), 'Publishing');
+  });
+
+  test('AwaitPublishing-Publishing-Publish-Failed', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitPublishing');
+    bridge.post('Publish');
+    expect(bridge.state(), 'Publishing');
+    bridge.post('Failed');
+    expect(bridge.state(), 'AwaitPublishing');
+  });
+
+  test('AwaitPublishing-Publishing-Publish-Succeeded', () {
+    MQTTBridge bridge = MQTTBridge(response);
+    bridge.setUnitTest();
+    expect(bridge.state(), 'Disconnected');
+    bridge.post('Connect');
+    expect(bridge.state(), 'Connecting');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitSubscribe');
+    bridge.post('Subscribe');
+    expect(bridge.state(), 'Subscribing');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitPublishing');
+    bridge.post('Publish');
+    expect(bridge.state(), 'Publishing');
+    bridge.post('Succeeded');
+    expect(bridge.state(), 'AwaitPublishing');
+  });
+
 }
